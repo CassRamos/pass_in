01e5612cdebfb54969c24045d4fe2a21e5fda32a
Cass <cassianoleaoramos@gmail.com>
2024-04-06 16:01:15 -0300

    Add exception handler for event not found

---
+++ src/main/java/cass/pass_in/Config/ExceptionEntityHandler.java
@@ -0,0 +1,15 @@
+package cass.pass_in.Config;
+
+import cass.pass_in.domain.event.exceptions.EventNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionEntityHandler {
+
+    @ExceptionHandler(EventNotFoundException.class)
+    public ResponseEntity handleEventNotFound(EventNotFoundException exception) {
+        return ResponseEntity.notFound().build();
+    }
+}
--- src/main/java/cass/pass_in/controllers/EventController.java
+++ src/main/java/cass/pass_in/controllers/EventController.java
@@ -1,9 +1,45 @@
 package cass.pass_in.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cass.pass_in.dto.attendee.AttendeesListResponseDTO;
+import cass.pass_in.dto.event.EventIdDTO;
+import cass.pass_in.dto.event.EventRequestDTO;
+import cass.pass_in.dto.event.EventResponseDTO;
+import cass.pass_in.services.AttendeeService;
+import cass.pass_in.services.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
-@RequestMapping("/event")
+@RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventController {
+
+    private final EventService eventService;
+    private final AttendeeService attendeeService;
+
+    @GetMapping("{id}")
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id) {
+        EventResponseDTO event = this.eventService.getEventDetail(id);
+        return ResponseEntity.ok(event);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder) {
+        EventIdDTO eventIdDTO = this.eventService.createEvent(body);
+
+
+        //build URI using DTO
+        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
+
+        return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+
+    @GetMapping("/attendees/{id}")
+    public ResponseEntity<AttendeesListResponseDTO> getEventAttendees(@PathVariable String id) {
+        AttendeesListResponseDTO attendeesListResponse = this.attendeeService.getEventsAttendee(id);
+        return ResponseEntity.ok(attendeesListResponse);
+    }
+
 }
