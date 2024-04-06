c84b3ac3e89fb0b6215ac0aaa6bceca896886537
Cass <cassianoleaoramos@gmail.com>
2024-04-06 15:58:15 -0300

    Create `Attendees` and `Events` DTO

---
+++ src/main/java/cass/pass_in/dto/event/EventIdDTO.java
@@ -0,0 +1,4 @@
+package cass.pass_in.dto.event;
+
+public record EventIdDTO(String eventId) {
+}
---
+++ src/main/java/cass/pass_in/dto/event/EventRequestDTO.java
@@ -0,0 +1,8 @@
+package cass.pass_in.dto.event;
+
+public record EventRequestDTO(
+        String title,
+        String details,
+        Integer maximumAttendees
+) {
+}
