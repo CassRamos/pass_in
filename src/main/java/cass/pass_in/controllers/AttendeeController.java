e2eb53cf4274f3cb8b1adce2645b51eea5401ddb
Cass <cassianoleaoramos@gmail.com>
2024-04-07 10:36:42 -0300

    Add verify subscription method and get attendee badge

--- src/main/java/cass/pass_in/controllers/AttendeeController.java
+++ src/main/java/cass/pass_in/controllers/AttendeeController.java
@@ -1,16 +1,26 @@
 package cass.pass_in.controllers;
 
+import cass.pass_in.dto.attendee.AttendeeBadgeDTO;
+import cass.pass_in.dto.attendee.AttendeeBadgeResponseDTO;
+import cass.pass_in.services.AttendeeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/attendees")
 public class AttendeeController {
 
-    @GetMapping
-    public ResponseEntity<String> testingGet() {
-        return ResponseEntity.ok("Success!!");
+    private final AttendeeService attendeeService;
+
+    @GetMapping("/{attendeeId}/badge")
+    public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder) {
+        AttendeeBadgeResponseDTO response = this.attendeeService.getAttendeeBadge(attendeeId, uriComponentsBuilder);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/cass/pass_in/services/AttendeeService.java
+++ src/main/java/cass/pass_in/services/AttendeeService.java
@@ -1,13 +1,18 @@
 package cass.pass_in.services;
 
 import cass.pass_in.domain.attendee.Attendee;
+import cass.pass_in.domain.attendee.exceptions.AttendeeAlreadyExistsException;
+import cass.pass_in.domain.attendee.exceptions.AttendeeNotFoundException;
 import cass.pass_in.domain.checkin.CheckIn;
+import cass.pass_in.dto.attendee.AttendeeBadgeDTO;
+import cass.pass_in.dto.attendee.AttendeeBadgeResponseDTO;
 import cass.pass_in.dto.attendee.AttendeeDetails;
 import cass.pass_in.dto.attendee.AttendeesListResponseDTO;
 import cass.pass_in.repositories.AttendeeRepository;
 import cass.pass_in.repositories.CheckInRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -42,6 +47,32 @@ public class AttendeeService {
                             checkedInAt);
                 }).toList();
         return new AttendeesListResponseDTO(attendeeDetailsList);
+    }
+
+    public void verifyAttendeeSubscription(String email, String eventId) {
+        Optional<Attendee> isAttendeeRegistered = this.attendeeRepository.findByEventIdAndEmail(eventId, email);
+        if (isAttendeeRegistered.isPresent())
+            throw new AttendeeAlreadyExistsException("Attendee is already registered!");
+    }
+
+    public void registerAttendee(Attendee newAttendee) {
+        this.attendeeRepository.save(newAttendee);
+    }
+
+    public AttendeeBadgeResponseDTO getAttendeeBadge(String attendeeId, UriComponentsBuilder uriComponentsBuilder) {
+        Attendee attendee = this.attendeeRepository
+                .findById(attendeeId)
+                .orElseThrow(() -> new AttendeeNotFoundException("Attendee not found with ID: " + attendeeId));
+
+        var uri = uriComponentsBuilder.path("/attendees/{attendeeId}/check-in").buildAndExpand(attendeeId).toUri().toString();
+
+        AttendeeBadgeDTO attendeeBadgeDTO = new AttendeeBadgeDTO(
+                attendee.getName(),
+                attendee.getEmail(),
+                uri,
+                attendee.getEvent().getId());
+
+        return new AttendeeBadgeResponseDTO(attendeeBadgeDTO);
     }
 
 }
